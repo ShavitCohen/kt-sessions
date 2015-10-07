c6e372761bdfc610c9f4d2b03aacc47bff413a83
Shavit Cohen <shavit.hit@gmail.com>
2015-10-07 08:57:18 +0300

    adding session3 practice files

---
+++ app/sessions/session3/practice/session3.practice.ctrl.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ktpracticeApp.controller:session3PracticeCtrl
+ * @description
+ * # session3PracticeCtrl
+ * Controller of the KTPracticeApp
+ */
+angular.module('KTPracticeApp')
+  .controller('session3PracticeCtrl', session3PracticeCtrl);
+
+session3PracticeCtrl.$inject = ['$scope','$http'];
+
+function session3PracticeCtrl($scope,$http) {
+
+  
+
+
+}
