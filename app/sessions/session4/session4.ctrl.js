621529b2dc1a265ef8bb95abfb5cd03149cdebd3
Shavit Cohen <shavit.hit@gmail.com>
2015-10-15 11:06:31 +0300

    uploading changes after session #4

--- app/sessions/session4/session4.ctrl.js
+++ app/sessions/session4/session4.ctrl.js
@@ -7,5 +7,26 @@ angular.module('KTPracticeApp')
 session4Ctrl.$inject = ["$scope"];
 
 function session4Ctrl($scope) {
+  $scope.state = {
+    danger: true,
+    success: false
+  };
+
+  $scope.getClass = function(){
+    return {'bg-danger': $scope.state.danger, 'bg-success': $scope.state.success};
+  };
+
+
+  $scope.changeState= function () {
+    $scope.state.danger = !($scope.state.danger);
+    $scope.state.success = !($scope.state.success);
+  };
+
+  $scope.getStyle = function(){
+    return {
+      'background-color': ($scope.state.danger) ? "red" : "blue"
+    }
+  };
+
 
 }
